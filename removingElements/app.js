2b43c8b85376a5cea2eb9999b4965c3e8582fb46
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-03 10:33:13 +0000

    removingElements

---
+++ removingElements/app.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('input');
+const button = document.getElementById('submit');
+/*add new const below*/
+
+/*from previous ex*/
+button.addEventListener('click', () => {
+    let listItem = document.createElement('li');
+
+    let list = document.getElementsByTagName('ul')[0];
+    listItem.textContent = input.value;
+    
+    list.appendChild(listItem);
+
+    input.value = '';
+})
